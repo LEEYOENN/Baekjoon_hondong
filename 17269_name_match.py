c98ef7d79105c0dfe8db7d7b330e7f62e9fe0edc
Lee Yeon <rofttm0033@gmail.com>
2024-09-19 02:10:54 +0900

    17269 code adjust

--- 17269_name_match.py
+++ 17269_name_match.py
@@ -1,36 +1,27 @@
 numOfStrokes = [ 3,2,1,2,4,3,1,3,1,1,3,1,3,2,1,2,2,2,1,2,1,1,1,2,2,1]
 N, M = map(int, input().split(' ')) 
-name1, name2 = input().strip().upper().split(' ')
-
-##print(name1, name2)
-
-# if len(name1) == 0 or len(name2) == 0:
-#     exit()
+name1, name2 = input().strip().split(' ')
 
 fullName = []
-if (len(name1) >= len(name2)) :
-    for i in range(len(name2)) :
+if (N >= M) :
+    for i in range(M) :
         fullName.append(name1[i])
         fullName.append(name2[i])
         
-    for i in range(len(name2), len(name1)) :
+    for i in range(M, N) :
         fullName.append(name1[i]) 
 else :
-    for i in range(len(name1)) :
+    for i in range(N) :
         fullName.append(name1[i])
         fullName.append(name2[i])
 
-    for i in range(len(name1), len(name2)) :
+    for i in range(N, M) :
         fullName.append(name2[i]) 
-##print(fullName)
 
-# fullName을 숫자로 변환
 fullNum = []
 for i in fullName :
     fullNum.append(numOfStrokes[ord(i) - ord('A')])
 
-    
-##print(fullNum)
 while len(fullNum) > 2:
     newFullNum = []
     for i in range(len(fullNum) - 1) :
