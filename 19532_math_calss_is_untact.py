389278a576e0b5c345b80ccbc7dad2bdde597770
Lee Yeon <rofttm0033@gmail.com>
2025-02-17 17:30:26 +0900

    Solve: 2231

--- 19532_math_calss_is_untact.py
+++ 19532_math_calss_is_untact.py
@@ -1,1 +1,9 @@
-print(7 // 2)
+a, b, c, d, e, f = map(int, input().split())
+flag = 0
+for x in range (-999, 1000) :
+    if flag: break
+    for y in range(-999, 1000) :
+        if (a*x) + (b*y) == c and (d*x) + (e*y) == f :
+            print(f"{x} {y}")
+            flag = 1
+            break
