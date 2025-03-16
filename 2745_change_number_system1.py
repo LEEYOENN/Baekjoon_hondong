d8975e67f9f7617cbe424e54c362b2306f990d9f
Lee Yeon <rofttm0033@gmail.com>
2025-03-16 15:55:13 +0900

    solved 2745

--- 2745_change_number_system1.py
+++ 2745_change_number_system1.py
@@ -1,4 +1,19 @@
-n, number_system = input().split()
-number_system = int(number_system)
+n, B = input().split()
+B = int(B)
 
-print(int(n, number_system))
+##print(int(n, B)) ##파이썬 내장 함수로 간단하게 가능
+
+rslt = 0 #변환된 10진법 값을 저장할 변수
+power = 0 #B의 지수(자리수)
+
+#숫자를 오른쪽부터 처리
+for i in reversed(n):
+    if '0' <= i <= '9':
+        num = ord(i) - ord('0') #문자 '0'을 빼서 본래 숫자만 남김
+    else:
+        num = ord(i) - ord('A') + 10 # 알파벳이라면 'A'를 빼고 10을 더해줌
+    
+    rslt += num * (B ** power) #B의 파워승 만큼 곱해서 자리수 반영영
+    power += 1 #위 자리수로 이동
+    
+print(rslt)
