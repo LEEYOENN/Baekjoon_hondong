2398339a289c584da6bbba50fc89eb6ff4e4d09c
LEEYOENN <rofttm0033@gmail.com>
2026-02-08 23:59:48 +0900

    1931: Greedy Algorithm

--- 1931_meetingroom_assigned.py
+++ 1931_meetingroom_assigned.py
@@ -4,3 +4,9 @@ input = sys.stdin.readline
 
 n = int(input())
 
+meeting = []
+
+for _ in range(n):
+    # 시작 시간(s), 끝나는 시간(e) 입력받아 리스트에 추가
+    s, e = map(int, input().split())
+    meeting.append((s, e))
