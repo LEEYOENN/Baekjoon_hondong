0ebecec631b49639b021fd9ad97df0381e715fa6
LEEYOENN <rofttm0033@gmail.com>
2026-02-10 19:01:26 +0900

    Hash Map

---
+++ two_sum.py
@@ -0,0 +1,15 @@
+def solution(nums, target):
+    num_map = {}
+
+    for i, num in enumerate(nums):
+        # 목표값에서 현재 값을 뺀 수가 딕셔너리에 있는지 확인
+        need_num = target - num
+
+        if need_num in num_map:
+            # 있다면 필요 수의 인덱스, 현재 수의 인덱스] 반환
+            return [num_map[need_num], i]
+        
+        # 없다면 현재 숫자의 인덱스를 딕셔너리에 저장
+        num_map[num] = i
+    
+    return []
