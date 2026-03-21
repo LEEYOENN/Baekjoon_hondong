98e39c8be31acaa1c561d9486aed59cb172d829d
LEEYOENN <rofttm0033@gmail.com>
2026-03-21 23:33:53 +0900

    programmers algorithm problem

---
+++ catch_a_thief.py
@@ -0,0 +1,13 @@
+import sys
+import ast
+
+input = sys.stdin.readline
+
+def solution(info, n, m):
+    answer = 0
+    return answer
+
+user_input = input()
+
+info = ast.literal_eval(user_input)
+print(info)
